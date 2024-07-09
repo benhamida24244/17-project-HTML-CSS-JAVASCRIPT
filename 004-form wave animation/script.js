22a9839dd49b78261a57bdc8bc4e79ca79df44f9
Benhamida Mohammed <mbenhamida136@gmail.com>
2024-07-09 19:17:41 +0100

    2 project Form wave animation and navigation rotating

---
+++ 004-form wave animation/script.js
@@ -0,0 +1,10 @@
+const Labels = document.querySelectorAll('.form-control label');
+
+Labels.forEach(label => {
+label.innerHTML = label.innerText.split("")
+.map(
+  (letter, idx) =>
+    `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+)
+.join("")
+});
